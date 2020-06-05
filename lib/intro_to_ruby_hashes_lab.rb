e88da62cb4836b89a2ce880e89f62dc7514d034d
Netaly Ramirez <21netaly@gmail.com>
2020-06-05 19:07:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,6 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hidden_leaf = { team_7: "Naruto, Sakura, Sasuke", team_8: "Hinata, Kiba, Shino"}
 end
 
 def pioneer
